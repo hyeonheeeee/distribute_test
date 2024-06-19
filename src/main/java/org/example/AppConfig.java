27289c412738e765367be66f030e83ca3d654981
hyeonhee <gitgusgml042933@gmail.com>
2024-06-19 16:46:53 +0900

    init

---
+++ src/main/java/org/example/AppConfig.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+// AppConfig.java
+@Configuration
+@ComponentScan(basePackages = "org.example")
+public class AppConfig {
+}
